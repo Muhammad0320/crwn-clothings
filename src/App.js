adddd87fb5a48bb09f873b87f420a8e1843223e7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-06-01 20:28:33 +0100

    Added the header component, shop and the contact link

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import ShopPage from "./pages/ShopPage/ShopPage.component";
 
 import { Route, Routes } from "react-router-dom";
 
+import HeaderComponent from "./components/header/header.components";
+
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <Routes>
         <Route exact path="/" Component={HomePage} />
         <Route path="/shop" Component={ShopPage} />
